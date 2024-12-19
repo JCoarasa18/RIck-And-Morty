314e6cb54ac0a0f803ea072b22b5aaa2b0a4c9ef
JCoarasa18 <jcoarasa18@gmail.com>
2024-12-19 10:26:40 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base:"https://JCoarasa18.github.io/RIck-And-Morty/",
+  plugins: [react()],
+})
